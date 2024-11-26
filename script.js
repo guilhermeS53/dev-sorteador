1151ca8d802d5c21eea45d76e5eaad475f7cce20
Guilherme Sousa <guilherme.gs17@outlook.com>
2024-11-25 22:04:39 -0300

    adicao de if para logica do js

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ function generateNumber() {
   const min = Math.ceil(document.querySelector(".input-min").value);
   const max = Math.floor(document.querySelector(".input-max").value);
 
-  const result = Math.floor(Math.random() * (max - min + 1)) + min;
-
-  document.querySelector(".result").value = result;
+  if (min >= max) {
+    alert("O valor mínimo deve ser menor que o valor máximo");
+  } else {
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    document.querySelector(".result").value = result;
+  }
 }
 
 buttonClassify.addEventListener("click", generateNumber);
